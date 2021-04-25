16c4303bb0c45888c0a1d1cd9a65676a3a22e4b7
Erkan Buelbuel <webia1.github@gmail.com>
2021-04-25 23:00:49 +0200

    Dashboard - Luke

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonService } from 'src/app/services/common.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  luke: unknown;
 
-  constructor() { }
+  constructor(private cs: CommonService) {}
 
   ngOnInit(): void {
+    this.initData();
   }
 
+  initData() {
+    this.cs.getLukeSkywalker().subscribe((d) => (this.luke = d));
+  }
 }
